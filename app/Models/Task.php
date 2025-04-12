e034b25d40bf5f4e100b6af8d96b6c3d18c4ea0e
Breayanxd <breayanmt10@hotmail.com>
2025-04-11 22:21:42 -0600

    feat: add Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "title",
+        "description",
+        "due_date",
+        "status",
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
