0cd85c9befdb4a37999f9923344dfebb9a2c2233
Breayanxd <breayanmt10@hotmail.com>
2025-04-13 18:51:56 -0600

    feat: add routes

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,21 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');*/
-
-/*Route::group(['prefix' => 'users'], function () {
-    Route::post('/', [UserController::class, 'store']);
-});*/
-
 Route::post("/register", [AuthController::class,"register"]);
 Route::post("/login", [AuthController::class,"login"]);
-Route::post("/logout", [AuthController::class,"logout"]);
-Route::delete("/users/{id}", [UserController::class,"destroy"]);
+
+
+Route::middleware("auth:sanctum")->group(function () {
+    //Auth
+    Route::get("/profile", [AuthController::class,"profile"]);
+    Route::post("/logout", [AuthController::class,"logout"]);
+
+    //User incomplete
+    Route::delete("/users/{id}", [UserController::class,"destroy"]);
+
+    //Tasks
+    
+
+});
+
 
