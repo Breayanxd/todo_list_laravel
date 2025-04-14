b94d53b19304a935ac4b829d96afb31f36e7f0a5
Breayanxd <breayanmt10@hotmail.com>
2025-04-13 18:34:22 -0600

    feat: login and register functions

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,8 +9,12 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 })->middleware('auth:sanctum');*/
 
-Route::group(['prefix' => 'users'], function () {
+/*Route::group(['prefix' => 'users'], function () {
     Route::post('/', [UserController::class, 'store']);
-});
+});*/
 
+Route::post("/register", [AuthController::class,"register"]);
+Route::post("/login", [AuthController::class,"login"]);
+Route::post("/logout", [AuthController::class,"logout"]);
+Route::delete("/users/{id}", [UserController::class,"destroy"]);
 
